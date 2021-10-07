37c2152827a39ec03d24dd7b81f1ce64aa013b4a
smc-dotexe <shanelgmcguire@gmail.com>
2021-10-06 23:22:48 -0600

    added lazy loading to elements comp

--- 6_comps/comps/src/app/app.module.ts
+++ 6_comps/comps/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component'
@@ -17,7 +16,6 @@ import { NotFoundComponent } from './not-found/not-found.component'
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
     CollectionsModule,
     AppRoutingModule,
   ],
