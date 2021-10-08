9251ea1f2dde89ee2bd7fad8e48897c3da543c0d
smc-dotexe <shanelgmcguire@gmail.com>
2021-10-07 22:24:39 -0600

    lazy loading / widgets / reusable components

--- 6_comps/comps/src/app/app-routing.module.ts
+++ 6_comps/comps/src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
     loadChildren:() => import('./elements/elements.module')
       .then(m => m.ElementsModule)
   },
+  {
+    path: 'collections',
+    loadChildren: () => import ('./collections/collections.module')
+      .then(m => m.CollectionsModule)
+  },
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
 ];
--- 6_comps/comps/src/app/elements/elements.module.ts
+++ 6_comps/comps/src/app/elements/elements.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    ElementsHomeComponent
+    ElementsHomeComponent,
+    PlaceholderComponent,
+    TimesDirective
   ],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
