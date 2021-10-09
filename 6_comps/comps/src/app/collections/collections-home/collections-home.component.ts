4b22c979173873f70a543d4bb8f15f70ced080e9
smc-dotexe <shanelgmcguire@gmail.com>
2021-10-09 11:37:50 -0600

    finished lazyloading/modules section

--- 6_comps/comps/src/app/collections/collections-home/collections-home.component.ts
+++ 6_comps/comps/src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer'},
+    { name: 'Gil', age: 26, job: 'Engineer' },
+    { name: 'Elyse', age: 30, job: 'Engineer' }
+  ];
 
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
