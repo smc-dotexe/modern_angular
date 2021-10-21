e1e1e05d73f7937fa7eddea07af4d3c73c64ebad
smc-dotexe <shanelgmcguire@gmail.com>
2021-10-20 22:54:46 -0600

    learning custom validators

---
+++ 12_mathform/mathform/src/app/equation/equation.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+import { MathValidators } from '../math-validators';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css']
+})
+export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  }, [MathValidators.addition]) // Reference to the function. No need for parantheses
+
+  constructor() { }
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit(): void {
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+}
